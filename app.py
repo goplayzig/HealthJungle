a522afc2f9e9952b7fb2d6809abadc70a15b0f10
goplayzig <goplayzig@gmail.com>
2024-09-02 23:41:41 +0900

    Add tailwindcss

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return 'This is Home!'
+   return render_template('login.html')
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5001,debug=True)
