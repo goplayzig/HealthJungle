3d16c232823f6841ca9a0f6d0d650ddfcd9e8553
goplayzig <goplayzig@gmail.com>
2024-09-03 22:08:38 +0900

    Add cookie logic

--- app.py
+++ app.py
@@ -8,18 +8,36 @@ app = Flask(__name__)
 
 client = MongoClient('localhost', 27017)
 db = client.healthyjungle
-SECRET_KEY = 'test'
+SECRET_KEY = '1g8t4@u%k4@!k9@jh#j0$'
 
 @app.route('/')
 def home():
-   return render_template('login.html')
+   token_receive = request.cookies.get('myToken')
+   print("token_receive", token_receive)
+   if len(token_receive.split('.')) != 3:
+    print("Invalid token format")
+    return render_template('login.html')
+   try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms = ['HS256'])
+        print("payload", payload)
+        user_info = db.user.find_one({"id": payload['id']})
+        print("cookie checked")
+      #   return render_template('signUp.html', nickname=user_info["name"])
+        return render_template('signUp.html')
+   except jwt.ExpiredSignatureError:
+      print("cookie expired")
+      return render_template('login.html')
+      #   return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+   except jwt.exceptions.DecodeError:
+      print("cookie undifined")
+      return render_template('login.html')
+      #   return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 @app.route('/api/register', methods=['POST'])
 def register():
    id = request.form['id_give']
    pw = request.form['pw_give']
    name = request.form['name_give']
-
    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
    db.user.insert_one({'id': id, 'pw': pw_hash, 'name': name})
    return jsonify({'result': 'success'})
@@ -35,9 +53,11 @@ def login():
     if result:
        payload = {
           'id': id,
-          'exp': datetime.datetime.now() + datetime.timedelta(seconds=5)   
+          'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)   
        }
-       token = jwt.encode(payload, SECRET_KEY, algorithm = 'HS256')
+       token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+       print('id, pw', id,pw)
+       print('token', token)
        return jsonify({'result': 'success', 'token': token})
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
