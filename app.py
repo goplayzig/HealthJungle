a6d66b9e8ddde854dac22d2d1de2929e71ed2595
goplayzig <goplayzig@gmail.com>
2024-09-03 14:36:50 +0900

    Add mongoDB

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.healthyjungle
 
 @app.route('/')
 def home():
