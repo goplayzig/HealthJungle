f9afb69ef6933198ae662faeceae14cd4e3a1586
goplayzig <goplayzig@gmail.com>
2024-09-04 15:53:51 +0900

    add get logic

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+from flask.json.provider import JSONProvider
+import json
 import hashlib
 import datetime
 import jwt
@@ -9,6 +11,23 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.healthyjungle
 SECRET_KEY = '1g8t4@u%k4@!k9@jh#j0$'
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, object):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+class CustomJSONProvider(JSONProvider):
+    def dumps(self, obj, **kwargs):
+        return json.dumps(obj, **kwargs, cls=CustomJSONEncoder)
+
+    def loads(self, s, **kwargs):
+        return json.loads(s, **kwargs)
+
+
+app.json = CustomJSONProvider(app)
 
 @app.route('/')
 def home():
@@ -52,30 +71,50 @@ def postLogin():
     pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
 
     result = db.user.find_one({'id': id, 'pw': pw_hash})
-
     if result:
        payload = {
           'id': id,
+          'name': result['name'],
           'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=5)   
        }
        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-       print('id, pw', id,pw)
-       print('token', token)
        return jsonify({'result': 'success', 'token': token})
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
     
 @app.route('/api/calendar', methods=['POST'])
 def postWorkOut():
-    type = request.form['type_give']
-    time = request.form['time_give']
-    userId = request.form['userId_give']
-    date = request.form['date']
-    result = db.workOut.insert_one({'type': type, 'time': time, 'userId': userId, 'date': date})
-    if result:
-        return jsonify({'result': 'success'})
-    else:
-        return jsonify({'result': 'fail', 'msg': '운동 기록에 실패하였습니다.'})
+    token_receive = request.cookies.get('myToken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms = ['HS256'])
+        userId = payload['id']
+        name = payload['name']
+        type = request.form['type_give']
+        time = request.form['time_give']
+        date = request.form['date']
+        memo = request.form['memo_give']
+        result = db.workOut.insert_one({'type': type, 'time': time, 'userId': userId, 'date': date, 'memo': memo, 'name': name})
+        if result:
+            return jsonify({'result': 'success'})
+        else:
+            return jsonify({'result': 'fail', 'msg': '운동 기록에 실패하였습니다.'})
+    except jwt.ExpiredSignatureError:
+        print("cookie expired")
+        return render_template('login.html')
+    except jwt.exceptions.DecodeError:
+        print("cookie undifned")
+        return render_template('login.html')
+    
+@app.route('/api/calendar', methods=['GET'])
+def getWorkOutByDate():
+        date = request.args.get('date')
+        if date:
+            workOut = db.workOut.find({'date': date})
+            workOuts = list(workOut)
+            print("WWW", workOuts)
+            return jsonify({'result': 'success', 'workOuts': workOuts}) 
+        else:   
+            return jsonify({'result': 'fail', 'message': 'Date parameter is missing'})
     
 @app.route('/signUp')
 def signUp():
